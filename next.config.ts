575b67d6cd255037e2fa030e8c7c9f0088c09a71
Abdul Rafay <abdullrrafay@gmail.com>
2024-12-30 21:21:09 +0500

    eighth commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.sanity.io'], // Sanity image domain
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840], // Define the possible image sizes
+    imageSizes: [16, 32, 48, 64, 96], 
   },
   env: {
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
